19b3e8330adbddff5ae1060c6997d2f4d1e8b6a2
Oderystella <ddott59@gmail.com>
2021-07-03 00:59:25 +0900

    feat: add url in logo, remove comment

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,5 @@ app_name = 'user'
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
-    # path('signup/detail/', SignupDeatilView.as_view() ,name='signup_detail'),
+    path('signup/detail/', SignupDeatilView.as_view() ,name='signup_detail'),
 ]
