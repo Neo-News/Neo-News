9ce85eacb76e4e97d12d0d030837595261d0814d
heejung-gjt <hee_9803@naver.com>
2021-07-02 16:40:35 +0900

    feat: create base.html
    - create index.html
    - add css

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from news.views import IndexDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexDetailView.as_view(), name='index'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView
 # Create your views here.
+
+
+class IndexDetailView(DetailView):
+
+  def get(self, request, **kwargs):
+    return render(request, 'index.html')
