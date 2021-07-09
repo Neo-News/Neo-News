92a4bab7dcf6b0778757cf87f742133c391c2238
Oderystella <ddott59@gmail.com>
2021-07-10 02:44:14 +0900

    feat: add DATABASES in settings.py
    - hide confidentials in my_settings.py and add it to gitignore

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import my_settings
 env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,12 +79,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_settings.DATABASES
+
 
 AUTH_USER_MODEL = 'user.User'
 
