3978cffa4a9b26ac9efee467b6e85adaa1f97a58
heejung-gjt <hee_9803@naver.com>
2021-07-10 19:01:41 +0900

    fix: user/models.py
    - change user inheritance

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from user.models import User,LoginLog, Category, Keyword
+from social.models import Like, Comment, ReComment
+from news.models import Article, Potal, Press, ArticleShare
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(LoginLog) 
+admin.site.register(Category)
+admin.site.register(Keyword)
+admin.site.register(Article)
+admin.site.register(Potal)
+admin.site.register(Press)
+admin.site.register(ArticleShare)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(ReComment)
