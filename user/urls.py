5cc59455cb29434ec5ce94f7c73cdce1a4b9cd8e
Oderystella <ddott59@gmail.com>
2021-07-11 21:47:37 +0900

    feat: add uploading image to aws-s3 in user/views.py
    - add url 'image-upload' in user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from user.views import UserLoginView, UserSignupView, SignupDeatilView, UserInforEditView
+from user.views import UserLoginView, UserSignupView, SignupDeatilView, UserInforEditView, ImageUpload
 
 app_name = 'user'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='signup'),
     path('signup/detail/', SignupDeatilView.as_view() ,name='signup_detail'),
     path('infor/edit/', UserInforEditView.as_view() ,name='infor-edit'),
+    path('infor/edit/image/', ImageUpload, name='image-upload'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView
-
+import boto3
+from boto3.session import Session
+from config.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME
+from datetime import datetime
+from .models import ProfileImage
 
 class UserLoginView(LoginView):
     template_name = 'login.html'
@@ -30,4 +34,35 @@ class SignupDeatilView(View):
 class UserInforEditView(View):
 
     def get(self, request, **kwargs):
-      return render(request, 'user-infor-edit.html')
+
+        img = ProfileImage.objects.filter(pk=4).first()
+
+        return render(request, 'user-infor-edit.html', {'img' : img})
+
+
+# 프로필 이미지 등록하는 함수
+def ImageUpload(request):
+    if request.method == 'POST':
+        file = request.FILES.get('img')
+        session = Session(
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name=AWS_S3_REGION_NAME
+        )
+        s3 = session.resource('s3')
+        now = datetime.now().strftime('%Y%H%M%S')
+        img_object = s3.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(
+            Key = now+file.name,
+            Body = file
+        )
+        s3_url = "https://neonews-s3.s3.ap-northeast-2.amazonaws.com/"
+        ProfileImage.objects.create(
+            title = now+file.name,
+            url = s3_url+now+file.name
+        )
+        return redirect('user:infor-edit')
+
+    # imgs = ProfileImage.objects.all()
+
+    # return render(request, 'user-infor.html', {'imgs' : imgs})
+    
