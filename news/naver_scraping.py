047e33a4b5a40e92336977b3c518ad2fdd5e370c
Oderystella <ddott59@gmail.com>
2021-07-22 01:13:17 +0900

    fix: completed naver_scraping
    - 아티클 테이블 str()에 self.title 추가
    - 프리뷰 이미지 없는 경우 에러났던 이슈를 try except코드 넣어서 계속 스크래핑 되도록 함
    - 스크래핑 후 DB에 안들어갔던 main 함수 코드 수정
    - 카테고리별로 스크래핑 가능하게 category_list 만듦

--- news/naver_scraping.py
+++ news/naver_scraping.py
@@ -1,13 +1,16 @@
+
 import requests
 from bs4 import BeautifulSoup
 from requests.api import head
 import time, datetime
+
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)) + '/app')))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 import django
 django.setup()
+
+
 from user.models import Category
 from news.models import Potal, Press, Article
 
@@ -17,6 +20,7 @@ date: 0720
 description: 
 네이버 뉴스 카테고리별로 하루치 스크래핑 하는 함수
 스크래핑한 데이터 DB에 넣어주기
+for문 때문에 depth가 깊어서 리팩토링 필요
 """
 
 # datetime -> time 객체로 만들어줌
@@ -30,20 +34,36 @@ def convert_datetime_to_timestamp(date_list):
     datetime_obj = datetime.datetime.strptime(date_string, '%Y-%m-%d %p %I:%M')
     return datetime.datetime.timestamp(datetime_obj)
 
+#{'정치' : 100, '경제' : 101, '사회' : 102, '문화' : 103, '세계' : 104, 'IT' : 105} 
+# naver_news_code = {
+#     '100' : [264, 265, 266, 267, 268],
+#     '101' : [259, 258, 261, 771, 260],
+#     '102' : [249, 250, 251, 254, 252],
+#     '103' : [241, 239, 240, 237, 238],
+#     '104' : [231, 232, 233, 234, 322],
+#     '105' : [731, 226, 227, 230, 732],
+# }
+naver_news_code = {
+    '100' : [265],
+    '101' : [259],
+    '102' : [249],
+    '103' : [239],
+    '104' : [231],
+    '105' : [226],
+}
 
-# 100 : 정치, 101: 경제, 102 : 사회, 103 : 생활/문화, 104 : 세계, 105 : IT/과학, 106 : X, 107 : 연예
-# category_list = [100, 101, 102, 103, 104, 105, 107]
-category_list = [103]
-sub_category_list = [241, 239]
-
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'}
+category_list = [key for key in naver_news_code.keys()]
 
 def parse_naver():
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'}
+    
     news_info = []
     for category in category_list:
         scraped_date = datetime.datetime.today().strftime("%Y%m%d")
-        for sub_category in sub_category_list:
+        for sub_category in naver_news_code[category]:
+            # time.sleep(5)
             for num in range(1,2):
+                # time.sleep(5)
                 url = f'https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid2={sub_category}&sid1={category}&date={scraped_date}&page={num}'
                 res = requests.get(url, headers=headers)
                 soup = BeautifulSoup(res.text, 'html.parser')
@@ -56,9 +76,8 @@ def parse_naver():
                 for news_ul in news_uls:
                     for news_li in news_ul:
                         try:
-                            time.sleep(1)
+                            time.sleep(3)
                             category = soup.select_one("#snb > h2 > a").text
-                            print(category)
                             press = news_li.select_one("dl > dd > span.writing").text
                             preview_img = news_li.select_one("li > dl > dt.photo > a > img")['src']
                             title = news_li.select_one("dl > dt:nth-child(2) > a").text.strip()
@@ -70,60 +89,71 @@ def parse_naver():
 
                             code = ref.split("aid=")[1]
                             content = article_soup.select_one("#articleBodyContents")  # 태그 타입임, str(content) 해줘야 함
-                            
                             date_str = article_soup.select_one("#main_content > div.article_header > div.article_info > div > span.t11").text
-                            # date_list = date_str.replace(".", " ").replace(":", " ").split(" ")
+                        
+                            data = {
+                                'category' : category,
+                                'press' : press,
+                                'code' : code,
+                                'preview_img' : preview_img,
+                                'title' : title,
+                                'content': str(content),
+                                'date' : date_str,
+                                'ref' : ref,
+                            }
+                            news_info.append(data)
 
                         except TypeError:
                             print('error')
-                            pass               
+                            pass
 
-                        data = {
-                            'category' : category,
-                            'press' : press,
-                            'code' : code,
-                            'preview_img' : preview_img,
-                            'title' : title,
-                            'content': str(content),
-                            'date' : date_str,
-                            'ref' : ref,
-                        }
-                        print(data)
-                        
-                        news_info.append(data)
     return news_info
-
 
 
 if __name__=='__main__':
     news_list = parse_naver()
+    print(len(news_list))
     print("스크래핑 성공")
-    # try:
-    for news in news_list:
-        press = Press.objects.filter(name=news['press']).first()
-        if not press:
-            press = Press.objects.create(name=news['press'])
-        article = Article.objects.filter(title=news['title']).first()
-        if not article:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    try:
+        for news in news_list:
             date_list = news['date'].replace(".", " ").replace(":", " ").split(" ")
             time_obj = convert_datetime_to_timestamp(date_list)
-            Article.objects.create(
-                category=Category.objects.filter(name=news['category']).first(),
-                press=press,
-                potal=Potal.objects.filter(name="네이버").first(),
-                code=news['code'],
-                preview_img=news['preview_img'],
-                title=news['title'],
-                content=news['content'],
-                date=news['date'],
-                ref=news['ref'],
-                counted_at = 0,
-                created_at=time_obj,
-            )
-            print("DB 넣기 성공")
-    # except:
-    #     print("DB 넣기 실패")
-    #     pass
+            category = ''
+            press = Press.objects.filter(name=news['press']).first()
+            if not press:
+                press = Press.objects.create(name=news['press'])
+            
+            article = Article.objects.filter(title=news['title']).first()
+            if not article:
+
+                if news['category'] == "생활/문화":
+                    category = Category.objects.filter(name='문화').first()
+                
+                if news['category'] == "IT/과학":
+                    category = Category.objects.filter(name='IT').first()
+                
+                if news['category'] == "세계":
+                    category = Category.objects.filter(name='국제').first()
+
+                Article.objects.create(
+                    category=category,
+                    press=press,
+                    potal=Potal.objects.filter(name="네이버").first(),
+                    code=news['code'],
+                    preview_img=news['preview_img'],
+                    title=news['title'],
+                    content=news['content'],
+                    date=news['date'],
+                    ref=news['ref'],
+                    counted_at = 0,
+                    created_at=time_obj,
+                )
+                print("DB 넣기 성공")
+
+    except:
+        print("DB 넣기 실패")
+        pass
 
 
 
