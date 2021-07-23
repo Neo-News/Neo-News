55d137fea674b5f73cf712b18e004e56ff8c8cef
heejung-gjt <hee_9803@naver.com>
2021-07-23 16:15:08 +0900

    fix: time function in utils
    - modify article model time function

--- news/models.py
+++ news/models.py
@@ -4,6 +4,7 @@ from behaviors import TimeStampable, Countable, Deleteable
 from user.models import User,Category, Keyword
 import time
 import datetime
+from utils import get_time_passed
 
 
 class Press(Deleteable):
@@ -46,33 +47,8 @@ class Article(TimeStampable, Countable):
 
     @property
     def created_string(self):
-      """
-      참고하셨으면 지우셔도 됩니당 :-)
-      스크래핑한 기사 시간을 현재 시간에서 빼주어 총 몇 초 차이가 나는지 계산하였고 나온 값을 아래와 같은
-      로직을 사용하여 ~초전 ~분전으로 나올 수 있게 하였습니다. 이때 다음기사는 년,월,일,시,분만 제공을 해주기 때문에
-      초를 없앤 후 계산하였습니다.datetime_format과 current_date부분이 현재 시간을 Time으로 받은뒤
-      초를 생략하여 계산한 로직입니다 (원래 time은 초까지 계산함) 
-      """
-      now = datetime.datetime.now()
-      datetime_format = now.strftime('%Y-%m-%d %H:%M:00')
-      current_date = time.mktime(time.strptime(datetime_format,'%Y-%m-%d %H:%M:%S'))
-
-      time_passed = float(current_date)-int(float(self.created_at))
-    #   print(time_passed)
-      if time_passed == 0:
-          return '1분 전'
-      if time_passed < 60:
-          return str(time_passed) + '분 전'
-      if time_passed//60 < 60:
-          return str(int(time_passed//60)) + '분 전'
-      if time_passed//(60*60) < 24:
-          return str(int(time_passed//(60*60))) + '시간 전'
-      if time_passed//(60*60*24) < 30:
-          return str(int(time_passed//(60*60*24))) + '일 전'
-      if time_passed//(60*60*24*30) < 12:
-          return str(int(time_passed//(60*60*24*30))) + '달 전'
-      else:
-          return '오래 전'  
+      created_time = get_time_passed(self.created_at)
+      return created_time
 
   
 class ArticleShare(models.Model):
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import time
+import datetime
+
 
 def context_infor(**kwargs):
   a = {}
@@ -9,19 +11,27 @@ def context_infor(**kwargs):
 
 
 # comment 시간 나타내기
-def get_time_passed(object):
-    time_passed = int(float(time.time()) - float(object))
+def get_time_passed(created_at):
+
+    now = datetime.datetime.now()
+    datetime_format = now.strftime('%Y-%m-%d %H:%M:00')
+    current_date = time.mktime(time.strptime(datetime_format,'%Y-%m-%d %H:%M:%S'))
+
+    time_passed = float(current_date)-int(float(created_at))
+    
+#   print(time_passed)
     if time_passed == 0:
-        return '1초 전'
+        return '1분 전'
     if time_passed < 60:
-        return str(time_passed) + '초 전'
+        return str(time_passed) + '분 전'
     if time_passed//60 < 60:
-        return str(time_passed//60) + '분 전'
+        return str(int(time_passed//60)) + '분 전'
     if time_passed//(60*60) < 24:
-        return str(time_passed//(60*60)) + '시간 전'
+        return str(int(time_passed//(60*60))) + '시간 전'
     if time_passed//(60*60*24) < 30:
-        return str(time_passed//(60*60*24)) + '일 전'
+        return str(int(time_passed//(60*60*24))) + '일 전'
     if time_passed//(60*60*24*30) < 12:
-        return str(time_passed//(60*60*24*30)) + '달 전'
+        return str(int(time_passed//(60*60*24*30))) + '달 전'
     else:
         return '오래 전'  
+
