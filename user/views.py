ee2524afd31c7346352a036be759cba2d902bc55
heejung-gjt <hee_9803@naver.com>
2021-12-07 23:31:08 +0900

    fix: post change password logic
    - 비밀번호 찾기 에러처리 error=True로 변경
    - 배포 url로 변경

--- user/views.py
+++ user/views.py
@@ -164,7 +164,7 @@ def kakao_login(request):
             raise SocialLoginException("User arleady logged in")
 
         client_id = os.environ.get("KAKAO_CLIENT_ID")
-        redirect_uri = "http://neonews.site/user/login/social/kakao/callback/"
+        redirect_uri = "https://neonews.site/user/login/social/kakao/callback/"
     
         return redirect(
             f"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}"
@@ -189,7 +189,7 @@ def kakao_login_callback(request):
             KakaoException("Can't get code")
 
         client_id = os.environ.get("KAKAO_CLIENT_ID")
-        redirect_uri = "http://neonews.site/user/login/social/kakao/callback/"
+        redirect_uri = "https://neonews.site/user/login/social/kakao/callback/"
         client_secret = os.environ.get("KAKAO_SECRET_KEY")
         request_access_token = requests.post(
             f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}&client_secret={client_secret}",
@@ -370,8 +370,7 @@ class UserInforAddView(LoginRequiredMixin, View):
             
             return JsonResponse({
                 'success':True,
-                # 'url': 'http://neonews.site/'
-                'url': 'http://127.0.0.1:8000/'
+                'url': 'https://neonews.site/'
                 })
 
 
@@ -404,7 +403,7 @@ class DeletePasswordView(LoginRequiredMixin ,View):
             UserService.update_active(data.user_pk)
             logout(request)
             messages.success(request, '회원탈퇴 완료 !')
-            context = context_info(error=error, url='http://neonews.site/')
+            context = context_info(error=error, url='https://neonews.site/')
             return JsonResponse(context)
 
     def _build_user_dto(self, request):
