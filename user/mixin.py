fdc4084b8bb2bdb6643049ac39b0597da95de1b4
heejung-gjt <hee_9803@naver.com>
2021-12-07 22:57:26 +0900

    refactor: modify email mixin logic
    - mixin class 메소드로 나누기
    - form, verify_email, verify_resend_email, send_email 메소드로 분리
    - email, resend_email view 로직 수정

--- user/mixin.py
+++ user/mixin.py
@@ -1,50 +1,64 @@
+from django.http.response import JsonResponse
 
-from utils import context_info
-
+from user.services import UserService, UserEmailVerifyService
 from user.tasks import send_email
 from user.models import User
-from user.services import UserService, UserEmailVerifyService
+from user.dto import ResendDto, SignupDto
+from utils import context_info
 
 
-class VerifyEmailMixin():
-    def send_verify_email(self, request, dto, form, email):
-        try:
-            if form.is_valid():
-                if email == 'new':
-                        user = UserService.create(dto)
-                        mail = dto.email
+class VerifyEmailMixin:
+    def verify_form(self, form):
+        if form.is_valid():
+            context = context_info(error=False)
 
-                elif email == 'again':      
-                    user = User.objects.get(email=dto.email)
-                    resend_email_user = User.objects.get(email=dto.resend_email)
-                    
-                    if user != resend_email_user:
-                        context = context_info(msg='이메일을 확인해주세요 !', error=True)
-                        
-                        return context
-
-                    user = UserService.update(dto.email, dto.resend_email)
-                    mail = dto.resend_email
-                
-                mail_title, message_data, mail_to = UserEmailVerifyService.verify_email_user(request, user.pk, mail)
-            
-                send_email.delay(mail_title, message_data, mail_to)
-            
-
-                context = context_info(msg='이메일을 인증해 회원가입을 완료하세요!', error=False)
-            
-                return context
-
+            return context
+        
+        else:
             error = form.non_field_errors()
+            context = context_info(msg=error, error=True)
 
-            if error:
-                print(error)
+            return context
 
-                context = context_info(msg=error, error=True)
-                
-                return context
+    def verify_email(self, dto:SignupDto):
+        try:
+            user = UserService.create(dto)
+            mail = dto.email
+
+            return {"user": user, "mail": mail}
 
         except User.DoesNotExist:
             context = context_info(msg='회원가입부터 해주세요', error=True)
-                        
+
+            return JsonResponse(context)
+    
+    def verify_resend_email(self, dto:ResendDto):
+        try:
+            user = User.objects.get(email=dto.email)
+            resend_email_user = User.objects.get(email=dto.resend_email)
+
+            if user != resend_email_user:
+                context = context_info(msg='이메일을 확인해주세요 !', error=True)
+
+                return JsonResponse(context)
+            
+            user = UserService.update(dto.email, dto.resend_email)
+            mail = dto.resend_email
+
+            return {"user": user, "mail": mail, "error":False}
+
+        except User.DoesNotExist:
+            context = context_info(msg="회원가입부터 해주세요", error=True)
+
             return context
+
+    def send_user_email(self, request, mail_info):
+        mail_title, message_data, mail_to = UserEmailVerifyService.user_email(request, mail_info["user"].pk, mail_info["mail"])
+        
+        send_email.delay(mail_title, message_data, mail_to)
+    
+        context = context_info(msg='이메일을 인증해 회원가입을 완료하세요!', error=False)
+        
+        return context
+
+
